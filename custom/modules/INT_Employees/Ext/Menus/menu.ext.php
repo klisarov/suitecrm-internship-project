02b1dab2413848d3618e6266ea2620ebca821947
klisarov <klis4rov@gmail.com>
2026-09-15 12:31:13 +0200

    fix duplicate module menu buttons by removing redundant ext menu files

--- custom/modules/INT_Employees/Ext/Menus/menu.ext.php
+++ custom/modules/INT_Employees/Ext/Menus/menu.ext.php
@@ -1,27 +1,4 @@
 <?php 
  //WARNING: The contents of this file are auto-generated
 
-
-global $mod_strings;
-
-if (ACLController::checkAccess('INT_Employees', 'edit', true)) {
-    $module_menu[] = [
-        'index.php?module=INT_Employees&action=EditView&return_module=INT_Employees&return_action=index',
-        $mod_strings['LNK_NEW_EMPLOYEE'],
-        'Create',
-        'INT_Employees',
-    ];
-}
-
-if (ACLController::checkAccess('INT_Employees', 'list', true)) {
-    $module_menu[] = [
-        'index.php?module=INT_Employees&action=index&return_module=INT_Employees&return_action=DetailView',
-        $mod_strings['LNK_EMPLOYEE_LIST'],
-        'List',
-        'INT_Employees',
-    ];
-}
-
-
-
 ?>
